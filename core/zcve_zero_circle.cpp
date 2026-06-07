629b3649be284566be48c7009fea6e6c19985785
TomAgacci <dementedmax@gmail.com>
2026-06-07 12:09:16 -0700

    Create zcve_zero_circle.cpp

---
+++ core/zcve_zero_circle.cpp
@@ -0,0 +1,5 @@
+#include "zcve_zero_circle.h"
+
+// This file intentionally left minimal.
+// All ZeroCircle math is inline for performance.
+// You may expand this into a full geometry module later.
